036cd7abb182dbd81edebd10306e8be309e9dace
Sumit Kumar Singh <sksingh.mits@gmail.com>
2016-05-19 22:30:22 -0500

    MVP Commit
    It's an MVP app

---
+++ js/app.js
@@ -0,0 +1,97 @@
+"use strict";
+
+function MapViewModel() {
+    // Variable Initialization
+    var self = this;
+    self.placeNames = ko.observableArray([]);
+    self.markers = [];
+    self.placeDescriptionList = [];
+    self.currentBounceIndex = -1;
+    self.currentInfoWindow = null;
+    self.query = ko.observable("");
+    self.temperature = ko.observable("Unavailable");
+
+
+    // Displaying Map in Canvas
+    var map = renderMap();
+    // Displaying Restaurant using google place APIs and getting the marker and place Description List
+    displayRestaurant(map).then(function (response) {
+        $('#restaurantList').css('height', window.innerHeight - 50 + 'px');
+        self.pushData(response);
+    }, function (error) {
+        alert('Error Displaying Restaurant. Please try again.');
+    });
+
+    // Pushing data in observable properties
+    self.pushData = function (response) {
+        if (response != null || response != undefined)
+            var placesList = response.places;
+        var placeDescriptionList = response.placeDescriptionList;
+        var markerList = response.markers;
+        for (var i = 0; i < placesList.length; i++) {
+            self.placeNames.push(placesList[i]);
+            self.markers.push(markerList[i]);
+            self.placeDescriptionList.push(placeDescriptionList[i]);
+        }
+    };
+
+    // OnClick Event Handler for Restaurant List
+    self.onClickRestaurant = function () {
+        var placeName = this;
+        var index = self.placeNames.indexOf(placeName);
+        self.animateMarker(index);
+        self.displayInfoWindow(index);
+    };
+
+    self.displayInfoWindow = function (index) {
+        var content = "No Data Found";
+        var infowindow = new google.maps.InfoWindow();
+        if (index != -1) {
+            var marker = self.markers[index];
+            if (self.currentInfoWindow != null)
+                self.currentInfoWindow.close();
+            var placeDetails = self.placeDescriptionList[index];
+            content = "<div><b> Website: <a href='" + placeDetails.website_url + ' \'>' + placeDetails.website_url + '</a> <br> Address: ' + placeDetails.street_address + '<br> Phone: ' + placeDetails.phone + "</b> </div>";
+        }
+        infowindow.setContent(content);
+        self.currentInfoWindow = infowindow;
+        infowindow.open(map, marker);
+    };
+
+    // Setting marker Animation
+    self.animateMarker = function (index) {
+        if (index != -1) {
+            if (self.currentBounceIndex != -1) {
+                self.markers[self.currentBounceIndex].setAnimation(null);
+            }
+            self.currentBounceIndex = index;
+            self.markers[index].setAnimation(google.maps.Animation.BOUNCE);
+        }
+    };
+
+    // Filtering
+    self.filter = function (data, event) {
+        var filter = self.query().toLowerCase();
+        self.placeNames.splice(0, self.placeNames().length);
+        for (var i = 0; i < self.placeDescriptionList.length; i++) {
+            if (self.placeDescriptionList[i].name.toLowerCase().indexOf(filter) != -1) {
+                self.placeNames.push(self.placeDescriptionList[i].name);
+                self.markers[i].setMap(map);
+            } else {
+                self.markers[i].setMap(null);
+            }
+        }
+    };
+
+    self.getTemperature = function () {
+        console.log('ca');
+        var url = 'http://api.openweathermap.org/data/2.5/weather?q=saintluis,mo&appid=bc7fdd8d57dee9e264567551c7ff0a18';
+        $.getJSON(url, function (data) {
+            self.temperature(Math.round(data.main.temp) - 273);
+        });
+    };
+    self.getTemperature();
+}
+
+ko.applyBindings(new MapViewModel());
+
---
+++ js/map.js
@@ -0,0 +1,77 @@
+'use strict';
+
+//Render the map in the div
+var renderMap = function () {
+
+    try {
+        $('#map-canvas').css('height', window.innerHeight - 50 + 'px');
+        var map = new google.maps.Map(document.getElementById('map-canvas'), {
+            center: {lat: 38.6068867843944, lng: -90.28923873853023},
+            zoom: 11,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+        return map;
+    }
+    catch (err) {
+        alert('Map cannot be loaded. Please try again');
+    }
+};
+
+// Call the Locu Api and display the locations on the google Map.
+// This call always executes after the map is loaded as it is called renderMap promise
+var displayRestaurant = function (map) {
+    return new Promise(function (resolve, reject) {
+        if (map == undefined || map == null) {
+            alert("Something is wrong with google Maps! Please try Again");
+            reject('-1');
+        }
+        var url = 'http://api.locu.com/v1_0/venue/search/?locality=saint+louis&category=restaurant&api_key=b827df72109febb7e8fd45f4ce3baaac2861f692';
+        $.ajax({
+            url: url,
+            type: "GET",
+            dataType: 'jsonp',
+            jsonp: 'callback',
+            success: function (data) {
+                return resolve(processData(data, map));
+            },
+            error: function (err) {
+                alert('err');
+                return reject(err);
+            }
+        });
+    });
+};
+
+var processData = function (data, map) {
+    var len = data.objects.length;
+    var restaurants = data.objects;
+    if (len > 13) {
+        len = 13;
+    }
+    var places = [];
+    var markers = [];
+    var placeDescriptionList = [];
+    var mapContent = {};
+    for (var i = 0; i < len; i++) {
+        var lat = restaurants[i].lat;
+        var long = restaurants[i].long;
+        var myLatLng = {lat: lat, lng: long};
+        var marker = new google.maps.Marker({
+            position: myLatLng
+        });
+        var placeDetails = {};
+        placeDetails.website_url = restaurants[i].website_url;
+        placeDetails.phone = restaurants[i].phone;
+        placeDetails.id = restaurants[i].id;
+        placeDetails.name = restaurants[i].name;
+        placeDetails.street_address = restaurants[i].street_address;
+        placeDescriptionList.push(placeDetails);
+        marker.setMap(map);
+        markers.push(marker);
+        places.push(restaurants[i].name);
+    }
+    mapContent.places = places;
+    mapContent.markers = markers;
+    mapContent.placeDescriptionList = placeDescriptionList;
+    return mapContent;
+};
